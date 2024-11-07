aadb955fd5a47ec2a818428bed8179f0621cb0dd
n4skx <nasksx@proton.me>
2024-11-08 02:30:02 +0700

    delete(flake.*,.gitignore): Clean repo

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  description = "A very basic flake";
-
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
-
-  outputs = { self, nixpkgs }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
-  };
-}
